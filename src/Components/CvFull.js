23a521ddffa64585424fbd400acea52843335f6b
BasakKamil <47579294+BasakKamil@users.noreply.github.com>
2019-11-26 15:10:53 +0100

    yoooo

--- src/Components/CvFull.js
+++ src/Components/CvFull.js
@@ -12,23 +12,46 @@ export class CvFull extends Component {
             this.morphing = anime({
                 targets: `${b}`,
                 translateX:[
-                    { value:  0 ,duration: 700,delay: 500 },
-                    { value:  this.width ,duration: 800,delay: 500 },
-                    { value: this.width/2,duration: 400,delay: 500 }
+                    { value:  0 ,duration: 200,delay: 500 },
+                    { value:  this.width ,duration: 200,delay: 200 },
+                    { value: this.width/2,duration: 200,delay: 200 }
                 ],
                 easing: 'easeInOutExpo',
                 loop: false,
     });
 }  
+
+
+animenext = (a,c) => {
+this.basket = anime({
+    targets:  `${a}`,
+    translateX: [
+        {value: window.innerWidth/2}, 
+        {value: window.innerWidth/3}
+     ],
+    translateY: [
+        {value: -100}
+    ],
+    scale: [2,4,6,4,2],
+    rotate: '2turn',
+    delay: `${c}`
+  });
+}
   componentDidMount(){
     this.animetwo('.WitamywCv');
-
+    this.animenext('.Start',500); 
 }
    
     render() {
         return (
+            <div>
                <div className="WitamywCv">
                 Witamy w Cv
+            </div>
+            <div className="Start">
+            O mnie :
+
+            </div>
             </div>
         )
     }
