b0842cc5401aeb2c83811ed04f1c233258f31c50
BasakKamil <47579294+BasakKamil@users.noreply.github.com>
2019-12-02 18:42:37 +0100

    new new

--- src/Components/Button/api.js
+++ src/Components/Button/api.js
@@ -8,32 +8,25 @@ class Api extends Component{
     };
     componentDidMount(){
 
-        // fetch(api)
-        // .then(response => response.json())
-        // .then(json => this.setState({ contacts: json.results }));
-        // }
-       
+ 
           
            
               // Check if the XMLHttpRequest object has a "withCredentials" property.
               // "withCredentials" only exists on XMLHttpRequest2 objects.
               const proxy = 'http://cors-anywhere.herokuapp.com/';
               const api = `${proxy}https://api.darksky.net/forecast/a18f321825c2c6503931cf827ff61142/37.8267,-122.4233`;
-              let fr = 'a/chf';
-              const frank = `${proxy}http://api.nbp.pl/api/exchangerates/rates/${fr}/?format=json`;
+              const frank = `${proxy}http://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json`;
               const dol = `${proxy}http://api.nbp.pl/api/exchangerates/rates/c/usd?format=json`;
-              let xhr = new XMLHttpRequest();
-              xhr.onload = function() {
-                var responseText = xhr.responseText;
-                console.log(responseText);
+   
+ 
                 fetch(api)
                 .then(response => response.json())
                 //  .then(data => console.log(data))
                  .then(data=>{
       
-                  const temperature = data.currently.temperature;
-                  console.log(temperature);
-                  document.querySelector('.Tempka').innerHTML = temperature;;  
+                  this.temperature = data.currently.temperature;
+                  console.log(this.temperature);
+        
     
                  });
         
@@ -53,11 +46,7 @@ class Api extends Component{
                 document.querySelector('.Dolce').innerHTML = course; 
                 });
                 
-               };
-              
-               xhr.onerror = function() {
-                 console.log('There was an error!');
-               };
+            
                
              
             
@@ -73,7 +62,7 @@ class Api extends Component{
         return(
                 <div>
                     <div contacts={this.state.contacts}></div>
-                    <p className= "col6  col-md-4 Tempka"></p>
+                          <p className= "col6  col-md-4 Tempka"></p>
                     <div> 
                          <p className="col6 col-md-4"> Obecny stan Dolara to: </p>
                          <p className="col6  col-md-4 Dolce">     </p>
