79da6a10c915f8161eaab68ed3221c21fac16ee5
BasakKamil <47579294+BasakKamil@users.noreply.github.com>
2019-12-14 18:12:45 +0100

    Go!

--- src/Components/LoginPanel.js
+++ src/Components/LoginPanel.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import firebaseApp from '../fbase.js';
+// import signIn from '../Components/actions/signin';
 import anime from 'animejs';
 import { Link }  from 'react-router-dom';
+// import {connect} from 'react-redux';
+import firebaseApp from '../fbase';
 
 
 export class LoginPanel extends Component {
@@ -15,13 +17,7 @@ export class LoginPanel extends Component {
 
     }
     
-    signup = (e) => {
-        e.preventDefault();
-        firebaseApp.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {           
-        }).then((u)=>{console.log(u)})
-        .catch((error)=>{console.log(error);
-        });
-    }
+
 
     authenticate = (event) => {
         event.preventDefault();
@@ -41,16 +37,26 @@ export class LoginPanel extends Component {
         })
     
     }
+
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     this.props.signIn(this.state);
+    // }
+
+   
+
     anime = (a) => {
-        
+        // const kamilaform = document.querySelector('.KamilaForm');
+        // console.dir(kamilaform);
+
         this.width = window.innerWidth;
         //    Animowanie wjezdzajacego Nagłówka h2
             this.morphing = anime({
                 targets: `${a}`,
                 translateX:[
-                    { value:  -this.width ,duration: 700,delay: 500 },
-                    { value: window.innerWidth/2,duration: 800,delay: 500 },
-                    { value: 0,duration: 400,delay: 500 }
+                    { value: window.innerWidth*1.1,duration: 1300,delay: 600 },
+                    { value:  this.width ,duration: 500,delay: 500 }
+        
                 ],
                 easing: 'easeInOutExpo',
                 loop: false,
@@ -65,7 +71,7 @@ export class LoginPanel extends Component {
 
     render() {
 
-        
+        // const {authError} = this.props;  
      
 
         return (
@@ -76,12 +82,27 @@ export class LoginPanel extends Component {
                         <input type="password" id="password" name="password" className="form-control" onChange={this.handleLoginChange} placeholder="********" value={this.state.password}></input>
                         <button type="submit" className="btn btn-danger KamilaButt">Log in</button> 
                         <p>lub <br/>Zarejstruj się !</p>
-                        <Link data-router-view="Signup" to="/Signup" className="btn btn-success">Signup</Link>
+                        <Link data-router-view="Signup" to="/Signup" className="btn btn-success"> Signup</Link>
+                        {/* <div>
+                        {authError ? <p>{authError}</p> :  null}
+                        </div> */}
                         {/* <button type="submit" className="btn btn-success KamilaButt" onClick={this.signup}>Signup</button>  */}
             </form> 
         </div>
         )
     }
 }
+// const mapStateToProps = (state) => {
+//     return{
+//         auth: state.firebase.auth,
+//         authError: state.auth.authError
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return{
+//         signIn: (creds) => dispatch(signIn(creds))
+//     }
+// }
 
 export default LoginPanel
---
+++ src/Components/newUser.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import firebaseApp from '../fbase'; 
+import {firestore} from 'redux-firestore';
+import axios from 'axios';
+// import { signUp } from './actions/signUp';
+// import {connect} from 'react-redux';
+// import {Redirect} from 'react-router-dom';
+
+
+
+export class newUserReg extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            firstname: '',
+            lastname: '',
+            adress: '',
+            city: '',
+            country: '',
+            phone: '',
+            users: [],
+            selectedFile: null
+        }
+    }
+   
+    signup = (e) => {
+        e.preventDefault();
+        firebaseApp.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((resp)=>{
+
+            return firestore.collection('users').doc(resp.user.uid).set({
+                firstname: this.state.firstname,
+                lastname: this.state.lastname
+                // initials: this.state.firstname[0] + this.state.lastname[0]
+            })
+        }).then(()=>{
+            console.log('SIGNUP_SUCCES');       
+         }).catch(err=> {
+            console.log('SIGNUP_ERROR', err);
+        })
+        
+    }
+
+
+
+    handleLoginChange = (event) => {
+        this.setState({
+            [event.target.id] : event.target.value
+    
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.signUp(this.state);
+        this.fileSelectedHandler();
+     
+    }
+
+
+    fileSelectedHandler = (e) => {
+        this.setState({
+            selectedFile: e.target.files[0]
+        })
+    }
+
+    fileUploadHandler = () => {
+        axios.post('');
+    }
+
+    render() {
+        // const {auth,authError} = this.props;
+        // if(auth.uid) return <Redirect to="/shop" />
+        return (
+            <div>
+                <form className="KamilaForm" onSubmit={this.handleSubmit}>
+                <h2 className="KamilaNagl">Zarejstruj się !</h2>
+                        <input type="text" placeholder="email" id="email" name="email" className="form-control" onChange={this.handleLoginChange} value={this.state.email}></input>
+                        <input type="password" id="password" name="password" className="form-control" onChange={this.handleLoginChange} placeholder="********" value={this.state.password}></input>
+                        <input type="text" placeholder="firstname" id="firstname" name="firstname" className="form-control" onChange={this.handleLoginChange} value={this.state.firstname}></input>
+                        <input type="text" placeholder="lastname" id="lastname" name="lastname" className="form-control" onChange={this.handleLoginChange} value={this.state.lastname}></input>
+                        <input type="text" id="adress" name="adress" className="form-control" onChange={this.handleLoginChange} placeholder="ul.Domaniecka 20A" value={this.state.adress}></input>
+                        <input type="text" id="city" name="city" className="form-control" onChange={this.handleLoginChange} placeholder="Warszawa" value={this.state.city}></input>
+                        <input type="file" onChange={this.fileSelectedHandler}/>
+                        <button type="submit" className="btn btn-danger KamilaButt" >Signup</button> 
+                         {/* {authError ? <p>{authError}</p> : null} */}
+               
+                
+            </form> 
+
+
+         
+        
+            </div>
+        )
+    }
+}
+
+// const auth = this.props.auth;
+
+
+// const mapStateToProps = (state) => {
+//     return{
+//         auth: state.firebase.auth,
+//         authError: state.auth.authError
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         signUp: (newUser) => dispatch(signUp(newUser))
+//     }
+
+// }
+
+export default newUserReg
